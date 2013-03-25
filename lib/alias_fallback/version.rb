bd47796de2a9e6a8e3bccff10e012a924716c30d
ShiningRay <shiningray.nirvana@gmail.com>
2013-03-25 21:33:18 +0800

    initial commit
    Signed-off-by: ShiningRay <shiningray.nirvana@gmail.com>

---
+++ lib/alias_fallback/version.rb
@@ -0,0 +1,3 @@
+module AliasFallback
+  VERSION = "0.0.1"
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,5 @@
+# -*- encoding : utf-8 -*-
+require 'rubygems'
+require 'test/unit'
+require 'active_support'
+require 'active_support/test_case'
